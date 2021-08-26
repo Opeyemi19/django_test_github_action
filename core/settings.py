4502c6b2c17be302bc8290c09c7ac41352a47615
opeyemi19 <sodikfatayo@gmail.com>
2021-08-26 16:16:55 +0000

    New configue

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5o@&sfkgafv80%98yv5dwukl8@ylq!16%tk3-qjtwqnv529m=*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fatayodjango.herokuapp.com']
 
 # Application definition
 
